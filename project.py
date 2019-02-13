99bfad82e614c24db21c1c3e71141f141dc099a2
ritwik1997 <46987314+ritwik1997@users.noreply.github.com>
2019-02-13 16:36:56 +0530

    Update project.py

--- project.py
+++ project.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import offsetbox
 from sklearn import manifold, datasets, decomposition, discriminant_analysis
+from mpl_toolkits.mplot3d import Axes3D
+
  
 digits = datasets.load_digits()
 X = digits.data
@@ -14,14 +16,14 @@ df = df.loc[:, :'n2047']
 df = df.loc[3:,]
 dataset = df.values
 
- 
+
 def embedding_plot(X, title):
     x_min, x_max = np.min(X, axis=0), np.max(X, axis=0)
     X = (X - x_min) / (x_max - x_min)
  
-    plt.figure()
-    ax = plt.subplot(aspect='equal')
-    sc = ax.scatter(X[:,0], X[:,1], lw=0, s=40)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    sc = ax.scatter(X[:,0], X[:,1], X[:,2], lw=0, s=40, c=X/1.)
  
     # shown_images = np.array([[1., 1.]])
     # for i in range(X.shape[0]):
@@ -32,10 +34,10 @@ def embedding_plot(X, title):
     plt.xticks([]), plt.yticks([])
     plt.title(title)
 
-X_PCA = decomposition.PCA(n_components = 2).fit_transform(dataset)
+X_PCA = decomposition.PCA(n_components = 3).fit_transform(dataset)
 #X_LDA = discriminant_analysis.LinearDiscriminantAnalysis(n_components = 2).fit_transform(X, y)
-X_tSNE = manifold.TSNE(n_components = 2).fit_transform(dataset)
-print X_PCA.shape, X_tSNE.shape
+X_tSNE = manifold.TSNE(n_components = 3).fit_transform(dataset)
+
 
 embedding_plot(X_PCA, "PCA")
 #embedding_plot(X_LDA, "LDA")
